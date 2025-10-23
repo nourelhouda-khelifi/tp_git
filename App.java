0bdb4b9670fa73e3d9b2f18b7f72bde174d45407
nourelhouda-khelifi <123408799+nourelhouda-khelifi@users.noreply.github.com>
2025-10-23 23:41:17 +0200

    conflit creation

--- App.java
+++ App.java
@@ -4,5 +4,6 @@ public class App{
         System.out.println("Hello, World!");
         System.out.println("Mehdi is good");
         System.out.println("Nour is also good");
+        System.out.println("I am learning Git");
     }
 }
