66382abfcdb2213d06f42ba93f4908cd3470bac8
trabelsi-Mehdi <164263512+trabelsi-Mehdi@users.noreply.github.com>
2025-10-23 23:35:44 +0200

    Mehdi's commit
    added a print out function

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App{
     //modified comment
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Mehdi is good");
     }
 }
