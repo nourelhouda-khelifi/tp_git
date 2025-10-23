1a177ceeae275282aaa3c2f662b6cbfac4b40742
nourelhouda-khelifi <123408799+nourelhouda-khelifi@users.noreply.github.com>
2025-10-23 23:37:44 +0200

    Nour mod 2

--- App.java
+++ App.java
@@ -3,5 +3,6 @@ public class App{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Mehdi is good");
+        System.out.println("Nour is also good");
     }
 }
